e7ec45c3626fe305fbf1a9b1d9ba9955f589c167
Denis Bagaev <bdv0186@hotmail.com>
2022-04-21 00:02:49 +0300

    Add user entity

--- src/Bdv.Sso.DataAccess/SsoContext.cs
+++ src/Bdv.Sso.DataAccess/SsoContext.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bdv.Sso.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bdv.Sso.DataAccess
 {
@@ -11,6 +7,13 @@ namespace Bdv.Sso.DataAccess
     {
         public SsoContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
         }
     }
 }
--- src/Bdv.Sso.Domain/Entities/User.cs
+++ src/Bdv.Sso.Domain/Entities/User.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Bdv.Domain.Abstractions;
+using Bdv.Domain.Abstractions.Sso;
+using Bdv.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bdv.Sso.Domain.Entities
 {
-    public class User
+    public class User : EntityBase<Guid>, IUser
     {
         /// <summary>
         /// User login
@@ -21,5 +24,35 @@ namespace Bdv.Sso.Domain.Entities
         /// </summary>
         [Column("login")]
         public string? Login { get; set; }
+
+        /// <summary>
+        /// User hashed password
+        /// </summary>
+        [Column("password")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Password salt
+        /// </summary>
+        [Column("password_salt")]
+        public string PasswordSalt { get; set; }
+
+        /// <summary>
+        /// If user should change password after next login
+        /// </summary>
+        [Column("is_need_change_password")]
+        public bool? IsNeedChangePassword { get; set; }
+
+        /// <summary>
+        /// If user confirmed e-mail by code
+        /// </summary>
+        [Column("is_email_confirmed")]
+        public bool? IsEmailConfirmed { get; set; }
+
+        /// <summary>
+        /// If user confirmed phone by sms/call
+        /// </summary>
+        [Column("is_phone_confirmed")]
+        public bool? IsPhoneConfirmed { get; set; }
     }
 }
